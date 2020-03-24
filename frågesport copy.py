812caba1ca373e26bbb4b149c5504f980d105744
abbidafor <54662964+abbidafor@users.noreply.github.com>
2020-03-24 16:03:17 +0100

    frågsport kod

--- frågesport copy.py
+++ frågesport copy.py
@@ -1,6 +1,8 @@
 # importerar en random funktion
 import random
 
+import requests
+
 # frågorna definerade till kortare variabler 
 Fråga1 = "Vem gav 1755 ut kokboksklassikern Hjelpreda I hushållningen för unga Fruentim(b)er?"
 Fråga2 = "När tillverkades första JAS gripen?"
@@ -10,48 +12,26 @@ Fråga4 = "Är norrland ett land?"
 # vi har 0 poäng från början
 poäng = 0
 
+url ='https://opentdb.com/api.php?amount=10&type=multiple'
+
 # dictionary/lista över frågorna, svarsalternativ och svar 
-frågor = [
-    {
-        'Fråga': Fråga1,
-        'Alternativ': [
-            "Selma Lagerlöv",
-            "Cajsa Warg",
-            "Lisa Ajax"
-        ],
-        'Svar': 'Cajsa Warg'
-    },
-    {
-        'Fråga': Fråga2,        
-        'Alternativ': [
-            "1985",
-            "1978",
-            "1982"
-        ],
-        'Svar': '1982'
-    },
-    {
-        'Fråga': Fråga3,
-        'Alternativ': [
-            "Shere Khan",
-            "Bagheera",
-            "Baloo"
-        ],
-        'Svar': 'Baloo'
-    },
-    { 
-        'Fråga': Fråga4,
-        'Alternativ': [
-            "Ja",
-            "På sätt o vis",
-            "Nej"
-        ],
-        'Svar': 'Nej'
-    }
-]
+frågor = []
+
+
+r = requests.get(url)
+response_dictionary = r.json()
+
+for result in response_dictionary['results']:
+    fråga = { } 
+    fråga['Fråga'] = result['question']
+    fråga['Alternativ'] = result['incorrect_answers']
+    fråga['Alternativ'].append(result['correct_answer'])
+    fråga['Svar'] = result['correct_answer']
+    frågor.append(fråga)  
+
 
 # skriver ut välkommen frasen
-print('Välkommen till Quizmas!')
+print('Välkommen till Quizmas!!!')
 
 # randomar frågorna 
 random.shuffle(frågor)
@@ -64,21 +44,13 @@ for fråga in frågor:
 # kollar vilket alternativ som är rätt 
 # hämtar index i listan med alternativ och kollar om det är 1 x eller 2
     index = fråga['Alternativ'].index(fråga['Svar'])
-    if index == 0:
-        fråga['RättAlternativ']  = '1'
-    elif index == 1:
-        fråga['RättAlternativ']  = 'X'
-    else:
-        fråga['RättAlternativ']  = '2'
+    fråga['RättAlternativ']  = index + 1
 
 # skriver ut frågan och salternativ
     print(fråga['Fråga'])
     print('Alternativ: ')
-
-# skriver ut 1, X eller 2 + svarsalternativ
-    print('1: ' + fråga['Alternativ'][0])
-    print('X: ' + fråga['Alternativ'][1])
-    print('2: ' + fråga['Alternativ'][2])
+    for index, alternativ in enumerate(fråga['Alternativ']):
+        print(str(index + 1) + ": " + alternativ)
 
 # kollar om inputen (det personen tror är rätt) stämmer med svaret. om den gör det skrivs rätt ut annars skrivs fel ut
 #.upper() gör att alla bokstäver blir stora
@@ -90,3 +62,6 @@ for fråga in frågor:
         print("Fel!")
     
 print("Du fick " + str(poäng) + " poäng!")
+
+
+
